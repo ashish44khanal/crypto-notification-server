7fb26a8e08c09908ea9c1d8f2bc6f3743e5d8098
ashish44khanal <ashish24khanal@gmail.com>
2022-10-09 13:43:20 +0545

    project bootstraped

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { config } from "dotenv";
+import express, {
+  Application,
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from "express";
+
+config();
+
+const PORT: number = Number(process.env.PORT) || 8080;
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response, nex: NextFunction) => {
+  res.status(200).json({
+    msg: "App is running.",
+    port: "5000",
+  });
+});
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  res.status(err.status || 5000).json({
+    status: err.status || 5000,
+    msg: err.message,
+  });
+};
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
